945fc14a087cacb7aff380907df299d18fb36daf
philipandreadis <33031735+philipandreadis@users.noreply.github.com>
2018-04-20 20:25:12 +0300

    mono kainouria gallery adapter kai gallery backend ta ipoloipa den ta piraksa

---
+++ app/src/main/java/com/example/xrhstos/bookapp/GalleryBackend.java
@@ -0,0 +1,29 @@
+package com.example.xrhstos.bookapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class GalleryBackend extends AppCompatActivity {
+    private RecyclerView galleryRecycler;
+    private RecyclerView.Adapter gAdapter;
+    private RecyclerView.LayoutManager gLayoutManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+        galleryRecycler = (RecyclerView) findViewById(R.id.gallery_recycler);
+
+        gLayoutManager = new LinearLayoutManager(this);
+        galleryRecycler.setLayoutManager(gLayoutManager);
+
+        gAdapter = new GalleryAdapter(myDataset);
+        galleryRecycler.setAdapter(mAdapter);
+
+
+
+    }
+}
